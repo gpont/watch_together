1284c0f588f8e91f64dd0329c9eb9557bca548cd
Evgenii Guzhikhin <gpont97@gmail.com>
2024-08-27 11:35:30 +0200

    Fix

--- src/middlewares/logger/logger.ts
+++ src/middlewares/logger/logger.ts
@@ -5,7 +5,7 @@ import { TMiddleware } from '../controllers.types';
 export const logger: TMiddleware = (handler) => (bot) => async (msg, match) => {
   if (process.env.DEBUG_OUTPUT === '1') {
     log(
-      `User: ${msg.from?.username}, ChatId: ${msg.chat.id}, Command: ${msg.text}`,
+      `User: ${msg.from?.username}, ChatId: ${msg.chat?.id}, Command: ${msg.text}`,
     );
     log('DB(before):');
     await listAllTablesData();
